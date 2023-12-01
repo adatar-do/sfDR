700bea837e09416d8e1b42136f1a06af7b312f8e
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2023-12-01 17:55:10 -0400

    doc: better general doc and readme file

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Percentage distribution by provinces of remittances received by the Dominican Republic
+#'
+#' @format ## `remesas_provincias`
+#' \describe{
+#'   \item{province}{Full name of the province}
+#'   \item{percentage}{Percentage of the total remittances received in 2010}
+#' }
+#' @source <https://www.bancentral.gov.do/a/d/2532-sector-externo>
+'remesas_provincias'
